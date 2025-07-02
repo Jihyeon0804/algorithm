a441393bd51a004a83b95f7e61223f1dbd4cf39a
Jihyeon0804 <cam080421@naver.com>
2025-07-02 16:22:06 +0900

    DP 배낭 문제

---
+++ src/A5DP/A05배낭문제.java
@@ -0,0 +1,13 @@
+package A5DP;
+
+// 주어진 데이터를 중복해서 사용할 수 있는 지 구분 필요
+public class A05배낭문제 {
+    public static void main(String[] args) {
+        // 동전 관련 문제처럼 풀 수 X
+        // 백준 - 평범한 배낭(12865)
+
+
+        // 백준 - 호텔(1106)
+
+    }
+}
