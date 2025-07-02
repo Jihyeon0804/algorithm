8082a0de1e31ef1a78bce7c1f7d910df38570cff
Jihyeon0804 <cam080421@naver.com>
2025-07-02 16:21:06 +0900

    Greedy 활용 문제

---
+++ src/A4greedy/A02그리디활용문제.java
@@ -0,0 +1,15 @@
+package A4greedy;
+
+public class A02그리디활용문제 {
+    public static void main(String[] args) {
+
+        // 프로그래머스 - 체육복
+        // 작은 index 에게 빌려줌
+
+        // 프로그래머스 - 구명보트
+        // 1) 정렬하기
+        // 2) 한 보트에 제한된 무게 내에서 작은 거 + 큰 거
+
+
+    }
+}
