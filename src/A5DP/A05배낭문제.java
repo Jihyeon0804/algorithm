d69da890f6af023d7df5ae3b31aac8ba7d1fc2ce
Jihyeon0804 <cam080421@naver.com>
2025-07-03 09:48:54 +0900

    DP 배낭 문제

--- src/A5DP/A05배낭문제.java
+++ src/A5DP/A05배낭문제.java
@@ -5,6 +5,7 @@ public class A05배낭문제 {
     public static void main(String[] args) {
         // 동전 관련 문제처럼 풀 수 X
         // 백준 - 평범한 배낭(12865)
+        // 배낭을 바깥 쪽 반복문, 무게를 안쪽 반복문에 배치해야 중복X
 
 
         // 백준 - 호텔(1106)
